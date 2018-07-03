2914de683db94a809ee1e807547fbb3ea5f30bbe
berkut97 <timur.bogdanov.97@inbox.ru>
2018-07-03 14:30:59 +0500

    Добавление создания Json-файлов

--- src/ru/berkut/Application.java
+++ src/ru/berkut/Application.java
@@ -1,10 +1,18 @@
 package ru.berkut;
 
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.TreeSet;
 
 import javax.xml.bind.JAXBException;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import ru.berkut.exception.DocumentExistsExeption;
 import ru.berkut.factory.FactoryGeneration;
@@ -52,15 +60,26 @@ public class Application {
 		  	* Создание отчета
 		 	*/
            setOfAuthors.forEach(author -> { 
+        	   List<Document> documentsOfAuthor = new ArrayList<Document>();
         	   System.out.println(" - " + author);        	        	
         	   for (Document d: Storage.data) {
         		   if (author.equals(d.getAuthor())) {
+        			    documentsOfAuthor.add(d);
                 		System.out.println("       - " + 
                 		Generator.type(d) + " от " +               				
                 		dateFormat.format(d.getRegDate()) + " Рег.№: " + d.getRegNumber());
                 		}
         		
-                	}       
+                	}    
+        	   Gson gson = new GsonBuilder()
+        			   				.setPrettyPrinting()
+        			   				.create();
+        			   		try (FileWriter writer = new FileWriter(System.getProperty("user.dir")+ File.separator + author + ".json")){						      
+        			   			String json = gson.toJson(documentsOfAuthor);
+        			   			writer.write(json);
+        			   		} catch (IOException e1) {
+        			   			e1.printStackTrace();
+        			   	}
             });
         }
 	
